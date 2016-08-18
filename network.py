1edfa2d06435fc38c73c1e0917034953c645f532
h-mayorquin <h.mayorquin@gmail.com>
2016-08-18 17:48:16 +0200

    Created a class for the BCPNN that is working for the play archive

--- network.py
+++ network.py
@@ -1,9 +1,10 @@
 import numpy as np
+from connectivity_functions import softmax
 
 
 class BCPNN:
-    def __init__(self,beta, w, s=None, o=None, a=None, z_pre=None, z_post=None,
-                 p_pre=None, p_post=None, p_co=None):
+    def __init__(self, beta, w, o=None, s=None, a=None, z_pre=None, z_post=None,
+                 p_pre=None, p_post=None, p_co=None, G=None, tau_m=None):
 
         # Connectivity
         self.beta = beta
@@ -19,10 +20,30 @@ class BCPNN:
         self.p_post = p_post
         self.p_co = p_co
 
+        # Parameters
+        self.G = G
+        self.tau_m = tau_m
 
-    def update_discrete(self):
-        
+        # If state variables and parameters are not initialized
+        if o is None:
+            self.o = np.random.rand(beta.size)
+        if s is None:
+            self.s = np.zeros_like(o)
+
+        if G is None:
+            self.G = 1.0
+
+        if tau_m is None:
+            self.tau_m = 1.0
 
 
-    def update_continuous(self):
+    def update_discrete(self, N=1):
+        for n in range(N):
+            self.s = self.beta + np.dot(self.w, self.o)
+            self.o = softmax(self.s, t=(1/self.G))
+
+    def update_continuous(self, dt=1.0):
+        self.s += (dt / self.tau_m) * (self.beta + np.dot(self.w, self.o) - self.s)
+        self.o = softmax(self.s, t=(1/self.G))
+
 
--- play.py
+++ play.py
@@ -1,10 +1,13 @@
 import numpy as np
 from connectivity_functions import get_beta, get_w, softmax
 from connectivity_functions import calculate_probability, calculate_coactivations
-# np.set_printoptions(suppress=True)
+from data_transformer import transform_normal_to_neural_single
+from data_transformer import transform_neural_to_normal_single
+from network import BCPNN
+np.set_printoptions(suppress=True)
 
-pattern1 = np.array((1, 0, 1, 0))
-pattern2 = np.array((0, 1, 1, 0))
+pattern1 = transform_normal_to_neural_single(np.array((1, 0, 0, 0, 0)))
+pattern2 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 1)))
 patterns = [pattern1, pattern2]
 
 P = calculate_coactivations(patterns)
@@ -13,20 +16,11 @@ p = calculate_probability(patterns)
 w = get_w(P, p)
 beta = get_beta(p)
 
-# Here we have the evolution
-T = 10
-dt = 0.1
+network = BCPNN(beta, w)
+print(network.o)
+network.update_discrete()
+print(network.o)
+network.update_discrete()
+print(network.o)
 
-tau_m = 1.0
-G = 1.0
-
-o = np.random.rand(p.size)
-m = np.zeros_like(o)
-
-for t in range(T):
-    # Update S
-    s = beta + np.dot(w, o)
-    # Evolve m, trailing s
-    m += (dt / tau_m) * (s - m)
-    # Softmax for m
-    o = softmax(m, t=(1/ G))
+x = transform_neural_to_normal_single(network.o)
