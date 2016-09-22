a4b40fafc19be9109c940bfe3fefff7e7428dc63
h-mayorquin <h.mayorquin@gmail.com>
2016-09-22 19:27:50 +0200

    modified the play to test

--- play.py
+++ play.py
@@ -17,10 +17,25 @@ np.set_printoptions(suppress=True)
 sns.set(font_scale=2.0)
 
 
+pattern1 = transform_normal_to_neural_single(np.array((1, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+pattern2 = transform_normal_to_neural_single(np.array((0, 1, 0, 0, 0, 0, 0, 0, 0, 0)))
+pattern3 = transform_normal_to_neural_single(np.array((0, 0, 1, 0, 0, 0, 0, 0, 0, 0)))
+pattern4 = transform_normal_to_neural_single(np.array((0, 0, 0, 1, 0, 0, 0, 0, 0, 0)))
+pattern5 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 1, 0, 0, 0, 0, 0)))
+pattern6 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 0, 1, 0, 0, 0, 0)))
+pattern7 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 0, 0, 1, 0, 0, 0)))
+pattern8 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 0, 0, 0, 1, 0, 0)))
+pattern9 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 0, 0, 0, 0, 1, 0)))
+pattern10 = transform_normal_to_neural_single(np.array((0, 0, 0, 0, 0, 0, 0, 0, 0, 1)))
+
+patterns = [pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9, pattern10]
+patterns = [pattern1, pattern2, pattern3]
+
 pattern1 = transform_normal_to_neural_single(np.array((1, 0, 0)))
 pattern2 = transform_normal_to_neural_single(np.array((0, 1, 0)))
 pattern3 = transform_normal_to_neural_single(np.array((0, 0, 1)))
-patterns = [pattern1, pattern2]
+
+patterns = [pattern1, pattern2, pattern3]
 
 P = calculate_coactivations(patterns)
 p = calculate_probability(patterns)
@@ -28,21 +43,42 @@ p = calculate_probability(patterns)
 w = get_w(P, p)
 beta = get_beta(p)
 
-# Parameters
+# Parameters and network intitiation
 tau_z_post = 0.240
 tau_z_pre = 0.240
 g_a = 0
 tau_a = 2.7
 
-nn = BCPNN(beta, w, p_pre=p, p_post=p, p_co=P, tau_z_post=tau_z_post, tau_z_pre=tau_z_pre,
-           tau_a=tau_a, g_a=g_a, M=2)
-
 dt = 0.01
 T = 1
 time = np.arange(0, T + dt, dt)
 
-dic_history = nn.run_network_simulation(time, save=True)
+distances_history = []
+closest_pattern = []
+final_equilibrium = []
+starting_point = []
+
+N = 10
+
+# Run and extract data
+for i in range(N):
+    nn = BCPNN(beta, w, p_pre=p, p_post=p, p_co=P, tau_z_post=tau_z_post, tau_z_pre=tau_z_pre,
+               tau_a=tau_a, g_a=g_a, M=2)
+
+    # Let's get the distances
+    start = nn.o
+    starting_point.append(start)
+    distances = {}
+    aux = [np.linalg.norm(start - pattern) for pattern in patterns]
+    distances_history.append({k:v for k,v in enumerate(aux)})
+    closest_pattern.append(aux.index(min(aux)))
+
+    # Run the simulation and get the final equilibrum
+    dic_history = nn.run_network_simulation(time, save=True)
+    final_equilibrium.append(transform_neural_to_normal_single(nn.o))
+
+
 
 # Plotting goes here
-from plotting_functions import plot_quantity_history
-plot_quantity_history(dic_history, 'o')
+#from plotting_functions import plot_quantity_history
+#plot_quantity_history(dic_history, 'o')\
