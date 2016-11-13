0faf18caa648007aa6e979d72f5b26fa0ee0ae43
h-mayorquin <h.mayorquin@gmail.com>
2016-11-13 20:07:13 +0100

    Updated notebook with examples of network size

--- play.py
+++ play.py
@@ -14,20 +14,21 @@ np.set_printoptions(suppress=True)
 
 # Patterns parameters
 hypercolumns = 4
-minicolumns = 20
-n_patterns = 5  # Number of patterns
+minicolumns = 10
+n_patterns = 10  # Number of patterns
 
 # Network parameters
 tau_z_pre = 0.500
 tau_z_post = 0.125
+tau_a = 0.5
 
 # Manager properties
 dt = 0.001
 T_training = 1.0
 T_ground = 0.5
-T_recalling = 30.0
+T_recalling = 10
 values_to_save = ['o', 'a', 'z_pre', 'z_post', 'p_pre', 'p_post', 'p_co', 'z_co', 'w']
-repetitions = 3
+repetitions = 1
 resting_state = False
 traces_to_plot = [1, 2, 3]
 
@@ -37,7 +38,7 @@ patterns = list(patterns_dic.values())
 patterns = patterns[:n_patterns]
 
 # Build the network
-nn = BCPNN(hypercolumns, minicolumns, tau_z_post=tau_z_post, tau_z_pre=tau_z_pre)
+nn = BCPNN(hypercolumns, minicolumns, tau_z_post=tau_z_post, tau_z_pre=tau_z_pre, tau_a=tau_a)
 
 
 # Build the manager
