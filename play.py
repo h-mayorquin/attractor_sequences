8c877069db3afa76241c00f5c818d019fef0a735
h-mayorquin <h.mayorquin@gmail.com>
2017-02-23 17:05:41 +0100

    Added a disambiguation notebook

--- play.py
+++ play.py
@@ -11,54 +11,52 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from network import BCPNN, NetworkManager, BCPNNFast, Protocol
 from plotting_functions import plot_state_variables_vs_time, plot_network_activity, plot_network_activity_angle
 from plotting_functions import  plot_adaptation_dynamics, plot_weight_matrix, plot_winning_pattern, plot_sequence
+from analysis_functions import calculate_recall_success
 # np.set_printoptions(suppress=True, precision=2)
 
 # Patterns parameters
-hypercolumns = 4
-minicolumns = 30
-n_patterns = 5  # Number of patterns
+hypercolumns = 16
+minicolumns = 20
 
 # Manager properties
 dt = 0.001
-T_recalling = 5.0
-values_to_save = ['o', 's', 'z_pre', 'z_post', 'p_pre', 'p_post', 'p_co', 'z_co', 'w',
-                  'p', 'k_d']
+T_recall = 5.0
+values_to_save = ['o']
+
 
 # Protocol
 training_time = 0.1
 inter_sequence_interval = 2.0
 inter_pulse_interval = 0.0
-epochs = 3
+epochs = 1
 
 # Build the network
 nn = BCPNNFast(hypercolumns, minicolumns)
-
-nn.k_inner = False
+nn.k_inner = True
 
 # Build the manager
 manager = NetworkManager(nn=nn, dt=dt, values_to_save=values_to_save)
 
-# Build the protocol for training
-
-patterns1 = [0, 1, 10, 11, 12]
-patterns2 = [2, 3, 10, 13, 14]
-epochs = 3
-
-chain = [patterns1, patterns2]
-
+# Build the protocol for
+patterns = [0, 1, 2, 3, 4]
 protocol = Protocol()
-protocol.cross_protocol(chain, training_time=training_time,
-                        inter_sequence_interval=inter_sequence_interval, epochs=epochs)
+protocol.simple_protocol(patterns_indexes=patterns, training_time=training_time,
+                         inter_pulse_interval=inter_pulse_interval, inter_sequence_interval=inter_sequence_interval,
+                         epochs=epochs)
+
 
 # Train
-if True:
-    epoch_history = manager.run_network_protocol(protocol=protocol, verbose=True, values_to_save_epoch=['w'])
-    plot_weight_matrix(nn, ampa=True, one_hypercolum=True)
-    plot_weight_matrix(nn, ampa=False, one_hypercolum=True)
-    plot_network_activity_angle(manager)
-    plot_winning_pattern(manager, remove=0.1)
-    plt.show()
+manager.run_network_protocol(protocol, verbose=False, values_to_save_epoch=None, reset=True, empty_history=True)
+plot_weight_matrix(nn, ampa=False, one_hypercolum=True)
+plot_weight_matrix(nn, ampa=True, one_hypercolum=False)
 
+manager.run_network_recall(T_recall=T_recall, T_cue=0.1, I_cue=0, reset=True, empty_history=True)
+plot_winning_pattern(manager)
+plot_network_activity_angle(manager)
+
+
+success = calculate_recall_success(manager, T_recall=T_recall, I_cue=0, T_cue=0.100, n=5, patterns_indexes=patterns)
+plt.show()
 
 
 
