404d98918cd3608e5125e14dda9b3d879239aac1
h-mayorquin <h.mayorquin@gmail.com>
2017-03-17 23:39:44 +0100

    Sequence capacity calculation

--- sequence_capacity.py
+++ sequence_capacity.py
@@ -5,10 +5,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # parameters
-m = 17
-k = 5
-o = 3
-p = 3
+m = 10
+k = 4
+o = 2
+p = 2
+
+verbose = True
 
 p_indicator = np.ones(m) * p
 position = 0
@@ -27,42 +29,72 @@ capacity += 1
 # Stack the array
 array = np.vstack((array, array_line))
 
+while position + k < m:
+    if verbose:
+        print('********')
+        print(position)
+        print(position + k)
+        print('**********')
+        # Lay another sequence
+    array_line = np.zeros(m, dtype='int')
+    # Create overlap dic with as many keys as sequences we have stored so far
+    overlap_dic = {index:0 for index in range(0, capacity)}
 
-# Lay the second sequence
-array_line = np.zeros(m, dtype='int')
-# Create overlap dic with as many keys as sequences we have stored so far
-overlap_dic = {index:0 for index in range(0, capacity)}
-
-# Move the position to the first position where p_indicator > 0
-for p_index, p_value in enumerate(p_indicator):
-    if p_value > 0:
-        position = p_index
-        break
-
-for position_i in range(k):
-
-    # Check that there is still p left to add
-    if p_indicator[position] == 0:
-        print('p flag')
-        position += 1
-
-    # Check overlap
-    for row in range(capacity):
-        # If overlap is bigger than o and that sequence is already there skip
-        if (overlap_dic[row] > o) and (array[row, position] == 1):
-            print('overlap flag')
-            position += 1
+    # Move the position to the first position where p_indicator > 0
+    for p_index, p_value in enumerate(p_indicator):
+        if p_value > 0:
+            position = p_index
             break
 
-    # Add the element
-    array_line[position + position_i] += 1
+    if position + k >= m:
+        print('break the while')
+        break
 
-    # Add overlap
-    for row in range(capacity):
-        # If there is an element
-        if array[row, position] == 1:
-            overlap_dic[row] += 1
+    position_i = position
+    for counter in range(k):
 
-capacity += 1
-array = np.vstack((array, array_line))
-pprint(array)
+        # Check that there is still p left to add
+        p_flag = True
+        while p_flag:
+            if p_indicator[position_i] == 0:
+                if verbose:
+                    print('p flag')
+                position_i += 1
+            else:
+                p_flag = False
+
+        # Check overlap
+        overlap_flag = True
+        while overlap_flag:
+            for row in range(capacity):
+                # If overlap is bigger than o and that sequence is already there skip
+                if verbose:
+                    print('--------------')
+                    print('row', row)
+                    print('position', position_i)
+                    print('overlap_dic', overlap_dic)
+                    print('array[row, position]', array[row, position_i])
+                if (overlap_dic[row] >= o) and (array[row, position_i] == 1):
+                    if verbose:
+                        print('overlap flag')
+                        print('--------------')
+                    position_i += 1
+                    overlap_flag = True
+                    break
+                else:
+                    overlap_flag = False
+
+        # Add the element
+        array_line[position_i] += 1
+        p_indicator[position_i] -= 1
+        position_i += 1
+
+        # Add overlap
+        for row in range(capacity):
+            # If there is an element
+            if array[row, position_i] == 1:
+                overlap_dic[row] += 1
+
+    capacity += 1
+    array = np.vstack((array, array_line))
+    pprint(array)
