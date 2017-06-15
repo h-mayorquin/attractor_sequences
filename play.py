b8256a199b190ce89bb151391e9813456b2a75a6
h-mayorquin <h.mayorquin@gmail.com>
2017-06-15 19:26:04 +0200

    Play

--- play.py
+++ play.py
@@ -19,10 +19,46 @@ from connectivity_functions import create_artificial_manager
 
 import pdb
 # pdb.set_trace()
-
 # Patterns parameters
 hypercolumns = 4
-minicolumns = 40
-n_patterns = 30
+minicolumns = 70
+n_patterns = 60
 
 dt = 0.001
+
+# Timings parameters
+tau_z_pre = 0.150
+tau_p = 5.0
+
+# Traiming parameters
+training_time = 0.100
+inter_sequence_interval = 1.0
+inter_pulse_interval = 0.0
+
+n = 5
+T_cue = 0.100
+T_recall = 10.0
+epochs = 3
+
+patterns_indexes = [i for i in range(n_patterns)]
+
+# Sequence protocol
+number_of_sequences = 3
+half_width = 2
+units_to_overload = [0, 1]
+
+# Build chain protocol
+chain_protocol = Protocol()
+sequences = chain_protocol.create_overload_chain(number_of_sequences, half_width, units_to_overload)
+
+beta = False
+value = 1.0
+inhbition = -1.0
+extension = 5
+decay_factor = 0.5
+sequence_decay  = 1.0
+
+manager = create_artificial_manager(hypercolumns, minicolumns, sequences, value=value, inhibition=inhbition,
+                                    extension=extension, decay_factor=decay_factor, sequence_decay=sequence_decay,
+                                    dt=dt, BCPNNFast=BCPNNFast, NetworkManager=NetworkManager, ampa=True, beta=beta)
+
